483d44ae0ae039189947668ac0ac150b4bf03921
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-08-20 10:55:18 +0530

    Setup login page and css

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,10 +1,35 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 
 const Login = () => {
 	return (
-		<div>
-			Login <Link to='/register'>Register Page</Link>
-		</div>
+		<Wrapper>
+			<form className='form'>
+				<Logo />
+				<h4>Login</h4>
+				<FormRow
+					type='email'
+					name='email'
+					labelText='email address'
+					defaultValue='anonguy@email.com'
+				/>
+				<FormRow
+					type='password'
+					name='password'
+					labelText='password'
+					defaultValue='password123'
+				/>
+				<button className='btn btn-block'>submit</button>
+				<button className='btn btn-block'>explore the app</button>
+				<p>
+					Not a member yet?
+					<Link to='/register' className='member-btn'>
+						Register
+					</Link>
+				</p>
+			</form>
+		</Wrapper>
 	);
 };
 
