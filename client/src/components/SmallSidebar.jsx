2a6f107627c4ea8b22009971f6b97e36281fe19e
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-08-20 17:30:23 +0530

    SmalSidebar setup and navlinks

--- client/src/components/SmallSidebar.jsx
+++ client/src/components/SmallSidebar.jsx
@@ -1,10 +1,29 @@
-import Wrapper from '../assets/wrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
 import { useDashboardContext } from '../pages/DashboardLayout';
+import Wrapper from '../assets/wrappers/SmallSidebar';
+import { NavLink } from 'react-router-dom';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
+import links from '../utils/links';
 
 const SmallSidebar = () => {
-	const data = useDashboardContext();
-	console.log(data);
-	return <Wrapper>SmallSidebar</Wrapper>;
+	const { showSidebar, toggleSidebar } = useDashboardContext();
+
+	return (
+		<Wrapper>
+			<div className={`sidebar-container ${showSidebar && `show-sidebar`}`}>
+				<div className='content'>
+					<button type='button' className='close-btn' onClick={toggleSidebar}>
+						<FaTimes />
+					</button>
+					<header>
+						<Logo />
+					</header>
+					<NavLinks />
+				</div>
+			</div>
+		</Wrapper>
+	);
 };
 
 export default SmallSidebar;
