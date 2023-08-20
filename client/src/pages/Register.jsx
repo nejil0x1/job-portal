9bdd99e91f9ff4e0b3b71db1b74e3a4595b07b4d
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-08-20 10:40:05 +0530

    Setup register page and formrow component

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,10 +1,48 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow, Logo } from '../components';
 
 const Register = () => {
 	return (
-		<div>
-			Register <Link to='/login'>Login Page</Link>
-		</div>
+		<Wrapper>
+			<form className='form'>
+				<Logo />
+				<h4>Register</h4>
+				<FormRow type='text' name='name' labelText='name' defaultValue='anon' />
+				<FormRow
+					type='text'
+					name='lastName'
+					labelText='last name'
+					defaultValue='guy'
+				/>
+				<FormRow
+					type='text'
+					name='location'
+					labelText='location'
+					defaultValue='asia'
+				/>
+				<FormRow
+					type='email'
+					name='email'
+					labelText='email address'
+					defaultValue='anonguy@email.com'
+				/>
+				<FormRow
+					type='password'
+					name='password'
+					labelText='password'
+					defaultValue='password123'
+				/>
+				{/* @toDo - add a confirmPassword component */}
+				<button className='btn btn-block'>submit</button>
+				<p>
+					Already a member?
+					<Link to='/login' className='member-btn'>
+						Login
+					</Link>
+				</p>
+			</form>
+		</Wrapper>
 	);
 };
 
