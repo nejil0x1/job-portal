96dad607a52e03d94b9c4b4591d1dd6768ee003e
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-08-29 23:43:24 +0530

    Stats container

--- client/src/components/StatsContainer.jsx
+++ client/src/components/StatsContainer.jsx
@@ -1,5 +1,48 @@
-const StatsContainer = () => {
-	return <h1>StatsContainer</h1>;
+import { FaSuitcaseRolling, FaCalendarCheck, FaBug } from 'react-icons/fa';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import StatItem from './StatItem';
+
+const StatsContainer = ({ defaultStats }) => {
+	const stats = [
+		{
+			title: 'pending applications',
+			count: defaultStats?.pending || 0,
+			icon: <FaSuitcaseRolling />,
+			color: '#f59e0b',
+			bgColor: '#fef3c7',
+		},
+		{
+			title: 'interviews scheduled',
+			count: defaultStats?.interviews || 0,
+			icon: <FaCalendarCheck />,
+			color: '#647acb',
+			bgColor: '#e0e8f9',
+		},
+		{
+			title: 'jobs declined',
+			count: defaultStats?.declined || 0,
+			icon: <FaBug />,
+			color: '#d66a6a',
+			bgColor: '#ffeeee',
+		},
+	];
+
+	return (
+		<Wrapper>
+			{stats.map(stat => {
+				return (
+					<StatItem
+						key={stat.title}
+						title={stat.title}
+						count={stat.count}
+						icon={stat.icon}
+						color={stat.color}
+						bgColor={stat.bgColor}
+					/>
+				);
+			})}
+		</Wrapper>
+	);
 };
 
 export default StatsContainer;
