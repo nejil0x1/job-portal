cdc4bc119abfa11886b46c793de5879c79aabab8
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-08-30 00:00:33 +0530

    Bar Carts container

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import Wrapper from '../assets/wrappers/ChartsContainer';
+
 const ChartsContainer = () => {
-	return <h1>ChartsContainer</h1>;
+	const [barChart, setBarChart] = useState(true);
+
+	return (
+		<Wrapper>
+			<h4>Monthly Applications</h4>
+			<button type='submit' onClick={() => setBarChart(!barChart)}>
+				{!barChart ? 'Bar Chart' : 'Area Chart'}
+			</button>
+			{barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+		</Wrapper>
+	);
 };
 
 export default ChartsContainer;
