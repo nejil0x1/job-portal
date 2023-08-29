31792204bff857e56c83398ea3a4b37d39f14874
Umang Utkarsh <umangutkarsh0024@gmail.com>
2023-08-29 10:57:46 +0530

    Populate db with tempData

---
+++ dbPopulate.js
@@ -0,0 +1,26 @@
+import { readFile } from 'fs/promises';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import Job from './models/JobModel.js';
+import User from './models/UserModel.js';
+
+try {
+	await mongoose.connect(process.env.MONGO_URL);
+
+	const user = await User.findOne({ email: 'test@test.com' });
+	const tempJobs = JSON.parse(
+		await readFile(new URL('./utils/tempData.json', import.meta.url))
+	);
+	const jobs = tempJobs.map(job => {
+		return { ...job, createdBy: user._id };
+	});
+	await Job.deleteMany({ createdBy: user._id });
+	await Job.create(jobs);
+	console.log('Success');
+	process.exit(0);
+} catch (error) {
+	console.log(error);
+	process.exit(1);
+}
